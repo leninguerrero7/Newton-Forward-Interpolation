2b6e1fe93d34dadfd0319a536658164f365e9898
Brandom Vasquez <negagen@aol.com>
2019-08-26 17:03:29 -0500

    MPI Beta

--- MPI/src/newtonforwardinterpolation.c
+++ MPI/src/newtonforwardinterpolation.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <mpi.h>
 #define MAXN 100
 #define ORDER 4
 #define MASTER 0
-main()
+
+int main(int argc, char *argv[])
 {
-    float ax[MAXN + 1], ay[MAXN + 1], diff[MAXN + 1][ORDER + 1], nr = 1.0, dr = 1.0, x, p, h, yp;
+    float ax[MAXN + 1], ay[MAXN + 1], diff[(MAXN + 1) * (ORDER + 1)], nr = 1.0, dr = 1.0, x, p, h, yp;
     int n, i, j, k;
     int taskid, /* task ID - also used as seed number */
         numtasks,
-        iterations, /* number of tasks */
-        rc;         /* return code */
+        stride, /* number of tasks */
+        rc;     /* return code */
 
     MPI_Status status;
     MPI_Init(&argc, &argv);
@@ -28,38 +30,48 @@ main()
         printf("\nEnter the value of x for which the value of y is wanted: \n");
         scanf("%f", &x);
         h = ax[1] - ax[0];
+
         MPI_Barrier(MPI_COMM_WORLD);
-        MPI_Bcast(&ax, [MAXN + 1], MPI_FLOAT, MASTER, MPI_COMM_WORLD);
-        MPI_Bcast(&ay, [MAXN + 1], MPI_FLOAT, MASTER, MPI_COMM_WORLD);
-        MPI_Bcast(&h, [MAXN + 1], MPI_FLOAT, MASTER, MPI_COMM_WORLD);
+        MPI_Bcast(&ax, MAXN + 1, MPI_FLOAT, MASTER, MPI_COMM_WORLD);
+        MPI_Bcast(&ay, MAXN + 1, MPI_FLOAT, MASTER, MPI_COMM_WORLD);
+        MPI_Bcast(&h, MAXN + 1, MPI_FLOAT, MASTER, MPI_COMM_WORLD);
     }
 
-    //now making the difference table
-    //calculating the 1st order of differences
-    for (i = 0; i <= n - 1; i++)
-        diff[i][1] = ay[i + 1] - ay[i];
+    for (i = taskid + 1; i <= n; i += numtasks)
+    {
+        diff[i + 1 * (MAXN + 1)] = ay[i + 1] - ay[i];
+    }
 
-    //now calculating the second and higher order differences
-    for (j = 2; j <= ORDER; j++)
+    for (j = taskid + 2; j <= ORDER; j += numtasks)
         for (i = 0; i <= n - j; i++)
-            diff[i][j] = diff[i + 1][j - 1] - diff[i][j - 1];
+            diff[i + j * (MAXN + 1)] = diff[(i + 1) + (j - 1) * (MAXN + 1)] - diff[i + (j - 1) * (MAXN + 1)];
 
-    //now finding x0
-    i = 0;
-    while (!(ax[i] > x))
-        i++;
+    if (taskid == MASTER)
+    {
+        i = 0;
+        while (!(ax[i] > x))
+            i++;
 
-    //now ax[i] is x0 and ay[i] is y0
-    i--;
-    p = (x - ax[i]) / h;
-    yp = ay[i];
+        //now ax[i] is x0 and ay[i] is y0
+        i--;
+        p = (x - ax[i]) / h;
+        yp = ay[i];
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
 
     //now carrying out interpolation
-    for (k = 1; k <= ORDER; k++)
+    for (k = taskid+1; k <= ORDER; k+=numtasks)
     {
         nr *= p - k + 1;
         dr *= k;
-        yp += (nr / dr) * diff[i][k];
+        yp += (nr / dr) * diff[i + (MAXN + 1) * k];
     }
-    printf("\nWhen x = %6.1f, corresponding y = %6.2f\n", x, yp);
+
+    if (taskid == MASTER)
+    {
+        printf("\nWhen x = %6.1f, corresponding y = %6.2f\n", x, yp);
+    }
+
+    return MPI_Finalize();
 }
